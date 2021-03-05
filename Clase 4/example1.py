8aed2063c7974f079509342f9bfe64dd15429379
shernandezz <santihernandezzapata@gmail.com>
2021-03-05 08:41:04 -0500

    Edited Example1

--- Clase 4/example1.py
+++ Clase 4/example1.py
@@ -3,18 +3,21 @@ import matplotlib.pyplot as plot
 
 df = pandas.read_csv("./Clase 4/data_example1.csv")
 
-df.plot(x="Time", y=["S1", "S3"], kind="line")
+df.plot(x="Time", y=["S1", "S4"], kind="line")
 #plot.show()
 
 x = df["S1"]
-y = df["S3"]
+y = df["S4"]
 
 N = len(x)
-rxy = numpy.sum((x - numpy.mean(x)) * (y - numpy.mean(y)))
+#rxy = numpy.sum((x - numpy.mean(x)) * (y - numpy.mean(y)))
+rxy = numpy.correlate(x - numpy.mean(x), y - numpy.mean(y))
 
 Prxy = rxy / ((N - 1) * numpy.sqrt(numpy.var(x) * numpy.var(y)))
 
-plot.title("Pearso Coeff" + str(round(Prxy, 2)))
+plot.title("Pearso Coeff " + str(numpy.round(Prxy, 2)))
+
+print(numpy.corrcoef(x, y))
 
 plot.show()
 
