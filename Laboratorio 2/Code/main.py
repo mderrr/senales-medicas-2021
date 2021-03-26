e82f9bfc9690f636c66714762cd3de5d80c7f5de
shernandezz <santihernandezzapata@gmail.com>
2021-03-25 23:34:56 -0500

    Laboratorio 2 terminado

--- Laboratorio 2/Code/main.py
+++ Laboratorio 2/Code/main.py
@@ -1,13 +1,17 @@
 import os
-os.chdir(os.getcwd() + "/Laboratorio 2/Code/")
-
 import math
 import platform
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 from statistics import mean
-from signal_generator import generateSignal
+
+# =========================================================== #
+#  IMPORTANTE MANTENER LA ESTRUCTURA DEL CODIGO, EL ARCHIVO   #
+#    'main.py' DEBE ESTAR DENTRO DE 'Laboratorio 2/Code'      #
+#  O PUEDE SER ABIERTO DIRECTAMENTE DESDE LA CARPETA 'Code'   #
+# =========================================================== #
+WORKING_DIRECTORY = "/Laboratorio 2/Code"
 
 POINT_2B_RESPONSE = ("RESPUESTA 2B: Al reducir en número de valores en la señal, esta pierde resolución y se hace más complicado diferenciar cuál de las frecuencias tiene "
                     "un gran parecido con la señal, hasta que al final pareciese que hay más de dos frecuencias dominantes.\nEste efecto es resultado de que al tomar "
@@ -29,22 +33,87 @@ POINT_3B_RESPONSE = ("RESPUESTA 3B: Los valores obtenidos en el numeral 2 y 3 so
                     "uno de estos se mide y luego se promedia. En cambio, en el método del espectro, se toma la frecuencia más parecida y se calculan las pulsaciones por "
                     "minuto en base a esta, haciendo parecer a la señal completamente estable y sin fluctuaciones, lo cual no es la realidad de cómo son los latidos del corazón.")
         
-WINDOWS_PLATFORM_NAME = "Windows"
+INSTANT_HEART_RATE_TITLE = "\nFrecuencias cardiacas instantaneas:"
+INSTANT_HEART_RATE_FORMAT = "{} Lpm"
+AVERAGE_HEART_RATE_MESSAGE = "Frecuencia cardiaca promedio:"
 
+SIGNAL1_CSV_LOCATION = "../signal1.csv"
+ECG_CSV_LOCATION = "../ecg_1min.csv"
+ECG_PEAKS_CSV_LOCATION = "../ecg_1min_rpeaks.csv"
+
+SIGNAL1_CSV_KEY = "Signal"
+ECG_CSV_KEY = "ECG"
+ECG_PEAKS_CSV_KEY = "R peaks"
+
+WINDOWS_PLATFORM_NAME = "Windows"
 WINDOWS_CLEAR = "cls"
 UNIX_CLEAR = "clear"
-
 SPACE = " "
 COMMA = ", "
 COMMA_ELLIPSIS = COMMA + "..."
+CENTER = "center"
+VERTICAL = "vertical"
+ENABLED = "Enabled"
+DISABLED = "Disabled"
+DASHED = "dashed"
+C, B = ("c", "b")
+
+GENERATED_SIGNAL, RECONSTRUCTED_SIGNAL, ORIGINAL_CSV_SIGNAL, PORTIONED_CSV_SIGNAL, ECG_CSV_SIGNAL = ("A", "B", "C", "D", "E")
+X, Y = ("X", "Y")
+WINDOW_TITLE, SUPTITLE, PLOT_TITLES, X_LABELS, Y_LABELS = ("WINDOW_TITLE", "SUPTITLE", "PLOT_TITLES", "X_LABELS", "Y_LABELS")
+
+SUBPLOT_INFO = {
+    GENERATED_SIGNAL: {
+        X: 1,
+        Y: 2,
+        WINDOW_TITLE: "Punto 1)",
+        SUPTITLE: "Sawtooth Signal",
+        PLOT_TITLES: ["Signal", "Frequency Spectrum"],
+        X_LABELS: ["Time (s)", "Frequency (Hz)"]
+    },
+    RECONSTRUCTED_SIGNAL: {
+        X: 1,
+        Y: 2,
+        WINDOW_TITLE: "Punto 1)",
+        SUPTITLE: "{} Point Signal Reconstruction",
+        PLOT_TITLES: ["Using IFFT", "Using Ecuation"],
+        X_LABELS: ["Time (s)"],
+        Y_LABELS: ["x(t)"]
+    },
+    ORIGINAL_CSV_SIGNAL: {
+        X: 2,
+        Y: 3,
+        WINDOW_TITLE: "Punto 2a)",
+        SUPTITLE: "Complete Frequency Spectrum",
+        X_LABELS: ["Frequency (Hz)"]
+    },
+    PORTIONED_CSV_SIGNAL: {
+        X: 2,
+        Y: 3,
+        WINDOW_TITLE: "Punto 2{})",
+        SUPTITLE: "Portioned Frequency Spectrums With Zero Padding {}",
+        PLOT_TITLES: ["From 1 to {}"],
+    },
+    ECG_CSV_SIGNAL: {
+        X: 1,
+        Y: 2,
+        WINDOW_TITLE: "Punto 3a)",
+        SUPTITLE: "ECG Frequency Spectrums",
+        PLOT_TITLES: ["Complete Frequency", "Zoomed In Frequency"],
+    }
+}
 
 FIRST_POINT, SECOND_POINT, THIRD_POINT = (0, 1, 2)
 
-
-SAMPLING_FREQUENCIES = [1024]
+SAMPLING_FREQUENCIES = [1024, 1000, 250]
 TOTAL_TIMES = [1]
 
+ECG_FREQUENCY_CUTTING_POINT = 20
+
 RECONSTRUCTION_COMPONENTS = 24
+
+if not (WORKING_DIRECTORY in os.getcwd()):
+    os.chdir(os.getcwd() + WORKING_DIRECTORY)
 
 def clear():
     os.system(WINDOWS_CLEAR if platform.system() == WINDOWS_PLATFORM_NAME else UNIX_CLEAR)
@@ -72,25 +141,25 @@ def firstPoint():
         time_array = np.arange(0, total_time, sampling_period)
         signal_array = np.zeros(number_of_samples)
 
-        signal_array[0 : number_of_samples // 2] = time_array[0 : number_of_samples // 2]
-        signal_array[number_of_samples // 2 : number_of_samples] = time_array[0 : number_of_samples // 2]
+        signal_array[: number_of_samples // 2] = time_array[: number_of_samples // 2]
+        signal_array[number_of_samples // 2 : number_of_samples] = time_array[: number_of_samples // 2]
 
         # Signal transforms to match example
         signal_array *= 2
         signal_array -= .5
 
         transformed_signal_array = np.abs(np.fft.fft(signal_array))
-        half_transformed_signal_array = transformed_signal_array[: number_of_samples // 2]
+        transformed_signal_array = transformed_signal_array[: number_of_samples // 2]
         frequency_array = (sampling_frequency // 2) * (np.arange(number_of_samples // 2) / (number_of_samples // 2))
 
-        figure, (subplot_original, subplot_transformed) = plt.subplots(ncols=2, num="Punto 1)")
-        figure.suptitle("Sawtooth Signal")
+        figure, (subplot_original, subplot_transformed) = plt.subplots(SUBPLOT_INFO[GENERATED_SIGNAL][X], SUBPLOT_INFO[GENERATED_SIGNAL][Y], num=SUBPLOT_INFO[GENERATED_SIGNAL][WINDOW_TITLE])
+        figure.suptitle(SUBPLOT_INFO[GENERATED_SIGNAL][SUPTITLE])
         subplot_original.plot(time_array, signal_array)
-        subplot_original.set_xlabel("Time (s)")
-        subplot_original.set_title("Signal")
-        subplot_transformed.plot(frequency_array, half_transformed_signal_array)
-        subplot_transformed.set_xlabel("Frequency (Hz)")
-        subplot_transformed.set_title("Frequency Spectrum")
+        subplot_original.set_title(SUBPLOT_INFO[GENERATED_SIGNAL][PLOT_TITLES][0])
+        subplot_original.set_xlabel(SUBPLOT_INFO[GENERATED_SIGNAL][X_LABELS][0])
+        subplot_transformed.plot(frequency_array, transformed_signal_array)
+        subplot_transformed.set_title(SUBPLOT_INFO[GENERATED_SIGNAL][PLOT_TITLES][1])
+        subplot_transformed.set_xlabel(SUBPLOT_INFO[GENERATED_SIGNAL][X_LABELS][1])
         plt.show()
 
         return time_array, signal_array
@@ -123,76 +192,65 @@ def firstPoint():
 
         return reconstructed_signal
 
-    def plotSignal(time_array, ifft_reconstructed_signal, ecuation_reconstructed_signal, base_signal_array, number_of_components):
-        figure, (subplot_ifft, subplot_ecuation) = plt.subplots(ncols=2, num="Punto 1)")
-        figure.suptitle("{} Point Signal Reconstruction".format(number_of_components))
-        figure.text(0.5, 0.04, "Time (s)", ha='center')
-        figure.text(0.04, 0.5, "x(t)", va='center', rotation='vertical')
+    def plotReconstructedSignals(time_array, ifft_reconstructed_signal, ecuation_reconstructed_signal, base_signal_array, number_of_components):
+        figure, (subplot_ifft, subplot_ecuation) = plt.subplots(SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][X], SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][Y], num=SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][WINDOW_TITLE])
+        figure.suptitle(SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][SUPTITLE].format(number_of_components))
+        figure.text(0.5, 0.04, SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][X_LABELS][0], ha=CENTER)
+        figure.text(0.04, 0.5, SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][Y_LABELS][0], va=CENTER, rotation=VERTICAL)
         subplot_ifft.plot(time_array, ifft_reconstructed_signal)
-        subplot_ifft.plot(time_array, base_signal_array, linestyle="dashed")
-        subplot_ifft.set_title("Using IFFT")
+        subplot_ifft.plot(time_array, base_signal_array, linestyle=DASHED)
+        subplot_ifft.set_title(SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][PLOT_TITLES][0])
         subplot_ecuation.plot(time_array, ecuation_reconstructed_signal)
-        subplot_ecuation.plot(time_array, base_signal_array, linestyle="dashed")
-        subplot_ecuation.set_title("Using Ecuation")
+        subplot_ecuation.plot(time_array, base_signal_array, linestyle=DASHED)
+        subplot_ecuation.set_title(SUBPLOT_INFO[RECONSTRUCTED_SIGNAL][PLOT_TITLES][1])
         plt.show()
 
-
     time_array, base_signal_array = generateSignal(sampling_frequency, total_time, number_of_samples)
-
     ifft_reconstructed_signal = ifftReconstruct(base_signal_array, RECONSTRUCTION_COMPONENTS, number_of_samples)
     ecuation_reconstructed_signal = ecuationReconstruct(number_of_samples, RECONSTRUCTION_COMPONENTS, base_frequency, base_signal_array, time_array)
+    plotReconstructedSignals(time_array, ifft_reconstructed_signal, ecuation_reconstructed_signal, base_signal_array, RECONSTRUCTION_COMPONENTS)
 
-    plotSignal(time_array, ifft_reconstructed_signal, ecuation_reconstructed_signal, base_signal_array, RECONSTRUCTION_COMPONENTS)
-    plt.show()
-
-# PUNTO 2
 def secondPoint():
-    signal1_csv = pd.read_csv("../signal1.csv")
-    signal_array = signal1_csv["Signal"]
-    sampling_frequency = 1000    
+    signal1_csv = pd.read_csv(SIGNAL1_CSV_LOCATION)
+    signal_array = signal1_csv[SIGNAL1_CSV_KEY]
+    sampling_frequency = SAMPLING_FREQUENCIES[SECOND_POINT]    
     number_of_samples = len(signal_array)
-    subplot_size_x, subplot_size_y = (2, 3)
 
     transformed_signal = np.abs(np.fft.fft(signal_array))
     transformed_signal = transformed_signal[:number_of_samples // 2]
     base_frequency = (sampling_frequency // 2) * (np.arange(number_of_samples // 2) / (number_of_samples // 2))
 
     def displayOriginal():
-        plt.figure("Punto 2a)")
-        plt.title("Complete Frequency Spectrum")
-        plt.xlabel("Frequency (Hz)")
+        plt.figure(SUBPLOT_INFO[ORIGINAL_CSV_SIGNAL][WINDOW_TITLE])
+        plt.title(SUBPLOT_INFO[ORIGINAL_CSV_SIGNAL][SUPTITLE])
+        plt.xlabel(SUBPLOT_INFO[ORIGINAL_CSV_SIGNAL][X_LABELS][0])
         plt.plot(base_frequency, transformed_signal)
         plt.show()
 
     def displayPortionedFrequencies(zero_padding=False):
-        figure, plots = plt.subplots(subplot_size_x, subplot_size_y, num="Punto 2{})".format("c" if zero_padding else "b"))
+        figure, plots = plt.subplots(SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][X], SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][Y], num=SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][WINDOW_TITLE].format(C if zero_padding else B))
 
-        for x in range(subplot_size_x):
-            for y in range(subplot_size_y):
+        for x in range(SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][X]):
+            for y in range(SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][Y]):
                 current_plot = plots[x, y]
-                one_dimensional_index = (x * subplot_size_y) + y
+                one_dimensional_index = (x * SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][Y]) + y
                 frequency_divider = 1 * pow(2, one_dimensional_index)
                 relative_number_of_samples = number_of_samples // frequency_divider
                 relative_signal = np.zeros((number_of_samples) if zero_padding else relative_number_of_samples)
-                relative_signal[:relative_number_of_samples] = signal_array[:relative_number_of_samples]
+                relative_signal[:relative_number_of_samples] = signal_array[:relative_number_of_samples]  
+                transformed_relative_signal = np.abs(np.fft.fft(relative_signal))
 
                 if zero_padding:
                     relative_frequency = base_frequency
-                    
+                    transformed_relative_signal = transformed_relative_signal[:number_of_samples // 2]
                 else:
                     relative_frequency = (sampling_frequency // 2) * (np.arange(relative_number_of_samples // 2) / (relative_number_of_samples // 2))
-
-                transformed_relative_signal = np.abs(np.fft.fft(relative_signal))
-
-                if not zero_padding:
                     transformed_relative_signal = transformed_relative_signal[:relative_number_of_samples // 2]
-                else:
-                    transformed_relative_signal = transformed_relative_signal[:number_of_samples // 2]
 
                 current_plot.plot(relative_frequency, transformed_relative_signal)
-                current_plot.set_title("From 1 to {}".format(relative_number_of_samples))
+                current_plot.set_title(SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][PLOT_TITLES][0].format(relative_number_of_samples))
 
-        figure.suptitle("Portioned Frequency Spectrums With Zero Padding {}".format("Enabled" if zero_padding else "Disabled"))
+        figure.suptitle(SUBPLOT_INFO[PORTIONED_CSV_SIGNAL][SUPTITLE].format(ENABLED if zero_padding else DISABLED))
         plt.show()
 
     displayOriginal()
@@ -203,35 +261,30 @@ def secondPoint():
     printResponse(POINT_2C_RESPONSE)
     displayPortionedFrequencies(True)
     
-# Point 3
 def thirdPoint():
-    ecg_csv = pd.read_csv("../ecg_1min.csv")
-    ecg_peaks_csv = pd.read_csv("../ecg_1min_rpeaks.csv")
-    ecg_array = ecg_csv["ECG"]
-    ecg_peaks_array = ecg_peaks_csv["R peaks"]
-    sampling_frequency = 250
-    
-    sampling_cutting_point = 20
-    sampling_divider = sampling_frequency / sampling_cutting_point
+    ecg_csv = pd.read_csv(ECG_CSV_LOCATION)
+    ecg_peaks_csv = pd.read_csv(ECG_PEAKS_CSV_LOCATION)
+    ecg_array = ecg_csv[ECG_CSV_KEY]
+    ecg_peaks_array = ecg_peaks_csv[ECG_PEAKS_CSV_KEY]
+    sampling_frequency = SAMPLING_FREQUENCIES[THIRD_POINT]
+    sampling_divider = sampling_frequency / ECG_FREQUENCY_CUTTING_POINT
     number_of_samples = len(ecg_array)
-    zoomed_number_of_samples = number_of_samples // sampling_divider
-    zoomed_ecg_array = []
+    zoomed_number_of_samples = int(number_of_samples // sampling_divider)
 
     def displayFrequencies():
         transformed_ecg_array = np.abs(np.fft.fft(ecg_array))
         half_transformed_ecg_array = transformed_ecg_array[:number_of_samples // 2]
-        zoomed_ecg_array = transformed_ecg_array[0: int(zoomed_number_of_samples)]
-
+        zoomed_ecg_array = transformed_ecg_array[:zoomed_number_of_samples]
         frequency_array = (sampling_frequency) * (np.arange(number_of_samples) / number_of_samples)
         half_frequency_array = (sampling_frequency / 2) * (np.arange(number_of_samples / 2) / number_of_samples / 2)
         zoomed_frequency_array = (sampling_frequency / sampling_divider)*(np.arange(zoomed_number_of_samples) / zoomed_number_of_samples)
 
-        figure, (subplot_original, subplot_zoomed) = plt.subplots(1, 2, num="Punto 3a)")
-        figure.suptitle("ECG Frequency Spectrums")
+        figure, (subplot_original, subplot_zoomed) = plt.subplots(SUBPLOT_INFO[ECG_CSV_SIGNAL][X], SUBPLOT_INFO[ECG_CSV_SIGNAL][Y], num=SUBPLOT_INFO[ECG_CSV_SIGNAL][WINDOW_TITLE])
+        figure.suptitle(SUBPLOT_INFO[ECG_CSV_SIGNAL][SUPTITLE])
         subplot_original.plot(half_frequency_array, half_transformed_ecg_array)
-        subplot_original.set_title("Complete Frequency")
+        subplot_original.set_title(SUBPLOT_INFO[ECG_CSV_SIGNAL][PLOT_TITLES][0])
         subplot_zoomed.plot(zoomed_frequency_array, zoomed_ecg_array)
-        subplot_zoomed.set_title("Zoomed In Frequency")
+        subplot_zoomed.set_title(SUBPLOT_INFO[ECG_CSV_SIGNAL][PLOT_TITLES][1])
 
         # Heart Rate
         max_frecuency_position = np.argmax(transformed_ecg_array)
@@ -243,20 +296,7 @@ def thirdPoint():
 
     def getIntantHeartRate():
         sampling_period = 1 / sampling_frequency
-        previous_peak = 0
         instant_heart_rate_array=[]
-
-        '''for peak in ecg_peaks_array:
-            current_peak_period = (peak * sampling_period) - previous_peak
-            
-            
-            current_heart_rate = current_peak_period * 60
-
-            instant_heart_rate_array.append(current_heart_rate)
-
-            previous_peak = (peak * sampling_period)
-            print("PERIOD: ", previous_peak)
-            print("INST HR: ", current_heart_rate)'''
 
         for i in range(len(ecg_peaks_array)):
             try:
@@ -269,17 +309,14 @@ def thirdPoint():
 
             instant_heart_rate_array.append(instant_heart_rate)
 
-        
-
         printResponse(POINT_3B_RESPONSE)
-        printShortList(instant_heart_rate_array, "\nFrecuencias cardiacas instantaneas:", "{} Lpm")
-        print("Frecuencia cardiaca promedio:", round(mean(instant_heart_rate_array)))
+        printShortList(instant_heart_rate_array, INSTANT_HEART_RATE_TITLE, INSTANT_HEART_RATE_FORMAT)
+        print(AVERAGE_HEART_RATE_MESSAGE, round(mean(instant_heart_rate_array)))
         
     displayFrequencies()
     getIntantHeartRate()
 
-
 clear()
 firstPoint()
-#secondPoint()
-#thirdPoint()
+secondPoint()
+thirdPoint()
