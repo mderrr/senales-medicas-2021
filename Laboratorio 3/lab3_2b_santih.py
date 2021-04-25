9d68f9e1e2094d62719f61406f1b31a23e4fd3a4
shernandezz <santihernandezzapata@gmail.com>
2021-04-24 23:01:41 -0500

    Modified Punto 2b

--- Laboratorio 3/lab3_2b_santih.py
+++ Laboratorio 3/lab3_2b_santih.py
@@ -13,21 +13,13 @@ HIGHLIGHT_BAND_COLOR = "gray"
 BANDS_FIGURE_TITLE = "Comparison of Transformed EEG bands"
 BANDS_FIGURE_SUPTITLE = "zoomed on a limit of {}Hz"
 
-EEG_BANDS_LIST = [
-    (0, 0, "Original Signal", 1),
-    (30, 0 , "Gamma", 3),
-    (12, 30 , "Beta", 5),
-    (8, 12 , "Alpha", 2),
-    (4, 7 , "Theta", 4),
-    (0, 4 , "Delta", 6)
-]
-
 PLOT_LIMIT_FREQUENCY = 65
 
 tick_formatter = plotTickFormatter(unit=HERTZ, sep="")
 noisy_eeg_signal = shared.getSignalFromFile(EEG_SIGNAL_FILE)
 
 def splitFftBands(eeg_bands_list, eeg_signal, start_unfiltered=True):
+    list_of_signals = []
     pyplot.figure(BANDS_FIGURE_TITLE).suptitle(BANDS_FIGURE_SUPTITLE.format(PLOT_LIMIT_FREQUENCY))
     axis = pyplot.subplot(3, 2, 1)
     pyplot.subplots_adjust(bottom=0.07, top=0.93, left=0.05, right=.95,  wspace=0.1, hspace=0.25)
@@ -35,6 +27,7 @@ def splitFftBands(eeg_bands_list, eeg_signal, start_unfiltered=True):
     for band in eeg_bands_list:
         frequency_array, eeg_band = shared.getEegBand(eeg_signal, EEG_SAMPLING_FREQUENCY, band, return_fft=True, do_not_filter=start_unfiltered)
         lower_bound, higher_bound, eeg_band_title, eeg_band_plot_index = band
+        list_of_signals.append(eeg_band)
         
         if higher_bound == 0: higher_bound = 1000
 
@@ -45,8 +38,9 @@ def splitFftBands(eeg_bands_list, eeg_signal, start_unfiltered=True):
         pyplot.plot(frequency_array, eeg_band)
 
         start_unfiltered = False
-    
+
     pyplot.show()
 
-shared.clear(POINT_TITLE)
-splitFftBands(EEG_BANDS_LIST, noisy_eeg_signal)
+if __name__ == "__main__":
+    shared.clear(POINT_TITLE)
+    splitFftBands(shared.EEG_BANDS_LIST, noisy_eeg_signal)
